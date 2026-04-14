3cdb5274d335d977c9b84277e76bc1a304f9b78e
zspri <19719195+zspri@users.noreply.github.com>
2026-04-14 23:28:36 +0200

    use correct function name

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,7 +3,7 @@ const { Resend } = require('resend');
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 const sendEmail = async (to, subject, html) => {
-    await resend.sendMail({
+    await resend.emails.send({
         from: `onboarding@resend.dev`,
         to,
         subject,
