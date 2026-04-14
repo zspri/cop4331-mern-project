ae1308dd07619e56048e6aa3781b0a60cd5a71a7
zspri <19719195+zspri@users.noreply.github.com>
2026-04-14 23:13:31 +0200

    update smtp config

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,11 +2,14 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (to, subject, html) => {
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
         },
+        connectionTimeout: 10000,
     });
 
     await transporter.sendMail({
