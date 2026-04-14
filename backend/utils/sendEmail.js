cc95cf6098609d9b3cd0e79a81f564558809a845
zspri <19719195+zspri@users.noreply.github.com>
2026-04-15 01:14:27 +0200

    derive email sender from env

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,12 +3,14 @@ const { Resend } = require('resend');
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 const sendEmail = async (to, subject, html) => {
-    await resend.emails.send({
-        from: `onboarding@resend.dev`,
+    const email = await resend.emails.send({
+        from: process.env.RESEND_EMAIL_ADDRESS,
         to,
         subject,
         html,
     });
+
+    console.log(email);
 };
 
 module.exports = sendEmail;
